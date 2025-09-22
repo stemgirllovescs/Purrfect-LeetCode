f645d9d652084fa16018e5561315d663187ca0ff
Katherine Liang <katherineliang100@utexas.edu>
2025-09-21 23:08:16 -0500

    stack

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,3 +1,20 @@
 class Solution(object):
     def isValid(self, s):
+        stack = []
+        closeToOpen = {"}" : "{", "]" : "[", ")" : "("}
+        for char in s:
+            # if it's a closed bracket
+            if char in closeToOpen:
+                # if we have a stack and top of stack is a matching open parenthesis
+                if stack and stack[-1] == closeToOpen[char]:
+                    # safely pop it
+                    stack.pop()
+                else:
+                    # mismatch, so return False early
+                    return False
+            else:
+                # it's an open parenthesis
+                stack.append(char)
         
+        return not stack
+    # Time O(N) Space = O(N)
