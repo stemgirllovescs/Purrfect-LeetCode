28ef57815f880062a7514ba7065cfa3130aeed29
Katherine Liang <katherineliang100@utexas.edu>
2025-10-20 10:14:27 -0500

    string

---
+++ Rotate String.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        return goal in (s + s) and len(s) == len(goal)
+
+        # time and space O(N)
